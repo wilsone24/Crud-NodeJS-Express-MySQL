a6e5256a6af61e61b6bdea22b58ae1b69193e5dd
wilsone24 <wilsone@uninorte.edu.co>
2024-09-05 20:47:17 -0500

    Refactor employees.routes.js

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { getEmployees, createEmployees, updateEmployees, deleteEmployees } from '../controllers/employees.controller.js'
-
+/*  */
 const router = Router();
 
 router.get('/employees',getEmployees)
@@ -12,3 +12,7 @@ router.put('/employees', updateEmployees)
 router.delete('/employees', deleteEmployees)
 
 export default router;
+/* 
+
+
+*/
